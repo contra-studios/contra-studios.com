cd83d257ae6040f8b748ae53a2ed5fd3df05309f
lricha18 <lrichardson3601@gmail.com>
2018-08-17 10:05:25 -0400

    Added pages for each item in nav-bar, footer now matches nav-bar, all pages are linked from nav-bar and footer, also our values and get in touch buttons have been linked to proper pages

---
+++ pages/values/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="foreground">
+        <div class="page-bar wrapper">
+          <Navigation></Navigation>
+        </div>
+      </div>
+    </header>
+
+    <div class="body wrapper">
+        <h1>Our Values Page</h1>
+    </div>
+
+    <Footer></Footer>
+
+  </div>
+
+</template>
+
+<script>
+import Navigation from '~/components/navigation.vue'
+import Footer from '~/components/footer.vue'
+
+export default {
+  components: {
+    Navigation,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+
+</style>
